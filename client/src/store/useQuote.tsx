eac7781befe013cdda111a5671d5fa778cd72dbf
jafetmora <138517617+jafetmora@users.noreply.github.com>
2025-08-15 18:54:56 -0600

    Revert "Redis Setup"

--- client/src/store/useQuote.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { QuoteContext } from "./QuoteContext";
-
-export function useQuote() {
-  const ctx = useContext(QuoteContext);
-  if (!ctx) throw new Error("useQuote must be used within a QuoteProvider");
-  return ctx;
-}
