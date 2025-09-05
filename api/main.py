971de05874b48c7ebcbf7a23784739b23119f6fe
Rmodadbaires <richard.modad@bairesdev.com>
2025-09-04 17:30:13 -0700

    hotfix on headers cloudfront issue

--- api/main.py
+++ api/main.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 
 from api.core.config import settings
 from api.routers import health, ws, quotes, auth
 
-
 app = FastAPI(title="IA-Agent API", version="0.1.0")
 
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=[
+        "localhost",
+        "cisco-poc-alb-640338878.us-east-2.elb.amazonaws.com",
+        "d2xwx7ojpy08cy.cloudfront.net",
+        "*.cloudfront.net"
+    ],
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,7 +25,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(health.router)
 app.include_router(ws.router)
